6f9b78f6ad3d710f8f898a8e3f82e04f6fc0176f
katia <ksymeonidh@gmail.com>
2023-01-30 12:01:29 +0100

    added faussing filter in WGS count

--- calculateCNVcorrelationWGS.py
+++ calculateCNVcorrelationWGS.py
@@ -3,6 +3,7 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 import scipy.stats
+import fargv
 from collections import defaultdict
 from matplotlib import pyplot as plt
 from sklearn.metrics import r2_score, mean_absolute_error, mutual_info_score, normalized_mutual_info_score
@@ -96,7 +97,7 @@ def calculatePopulationSomies(atac_dict,wgs_dict):
     #print(sort_common_keys)
     counts=0
     for k in sort_common_keys:
-        if k[0]==22: #selecting chromosome
+        if k[0]==9: #selecting chromosome
         #if k[0]!=0:  # selecting for all chromosomes
             counts=counts+1
             #print(wgs_dict[k])
@@ -114,7 +115,7 @@ def calculatePopulationSomies(atac_dict,wgs_dict):
     return(loss_wgs,base_wgs, gain_wgs, loss_atac, base_atac, gain_atac)
 
 def calculatePopulationSomiesWGS(atac_dict,wgs_dict):
-    #new_atac_dict = {k: [sum(atac_dict[k])/len(atac_dict[k])] for k in atac_dict.keys() if k[0]=="chr22"}
+    #new_atac_dict = {k: [sum(atac_dict[k])/len(atac_dict[k])] for k in atac_dict.keys() if k[0]==9}
     new_atac_dict = {k: [sum(atac_dict[k]) / len(atac_dict[k])] for k in atac_dict.keys()}
     common_keys = set(wgs_dict).intersection(new_atac_dict) #filtering for the common CNV locations between the two datasets
     sort_common_keys=sorted(common_keys)
@@ -123,30 +124,31 @@ def calculatePopulationSomiesWGS(atac_dict,wgs_dict):
     common_atac_dict = {key: new_atac_dict[key] for key in sort_common_keys}
     return(common_wgs_dict, common_atac_dict)
 
-
 #Function for calculating different metrics between the two datasets and creating a line plot of the pseudoibulk data
-def createLinePlotAneufinder(common_wgs_dict, common_atac_dict):
+def createLinePlotAneufinder(common_wgs_dict, common_atac_dict,gaussian_sigma=0,filter_edges="constant"):
     wgs_list=([i for i in common_wgs_dict.values()])
     atac_list= [i for i in common_atac_dict.values()]
     wgs_array = np.hstack(wgs_list)
+    smoothed_wgs_array = scipy.ndimage.gaussian_filter1d(wgs_array, gaussian_sigma, axis=- 1, order=0, output=None,
+                                                   mode=filter_edges)
     atac_array = np.hstack(atac_list)
     #atac_array=np.asarray(tuple(common_atac_dict.values()))
     #wgs_array = np.asarray(tuple(common_wgs_dict.values()))
     print("MIS:", normalized_mutual_info_score(atac_array,wgs_array, average_method='min'))
-    print("Pearson Correlation : ", scipy.stats.pearsonr(standarize(atac_array),standarize(wgs_array)))
-    print("Spearman Correlation : ", scipy.stats.spearmanr(atac_array, wgs_array)[0])
-    print("Kendall Correlation : ", scipy.stats.kendalltau(atac_array, wgs_array)[0])
+    print("Pearson Correlation : ", scipy.stats.pearsonr(standarize(atac_array),standarize(smoothed_wgs_array)))
+    print("Spearman Correlation : ", scipy.stats.spearmanr(atac_array, smoothed_wgs_array)[0])
+    print("Kendall Correlation : ", scipy.stats.kendalltau(atac_array, smoothed_wgs_array)[0])
 
-    difference_array = np.subtract(atac_array, wgs_array)
+    difference_array = np.subtract(atac_array, smoothed_wgs_array)
     squared_array = np.square(difference_array)
     mse = squared_array.mean()
     print("Mean Square Error: ",squared_array.mean())
 
-    print("R square: ",r2_score(atac_array, wgs_array))
-    print("Mean Absolute Error: ",mean_absolute_error(atac_array,wgs_array))
-    x = list(range(len(wgs_array)))
+    print("R square: ",r2_score(atac_array, smoothed_wgs_array))
+    print("Mean Absolute Error: ",mean_absolute_error(atac_array,smoothed_wgs_array))
+    x = list(range(len(smoothed_wgs_array)))
     plt.plot(x,(standarize(atac_array)), color='#df979e', label="ATAC")
-    plt.plot(x,(standarize(wgs_array)), color='#98d1d1', label="WGS")
+    plt.plot(x,(standarize(smoothed_wgs_array)), color='#98d1d1', label="WGS")
     borders_colorep1 = [0, 2109, 4392, 6241, 8068,9661, 11085, 12484, 13785, 14854, 16070, 17287, 18527, 19236, 20058, 20758, 21504, 22212, 22836, 23312, 23867, 24172, 24482]
     #plt.plot(x,((atac_array)), color='#df979e', label="ATAC")
     #plt.plot(x,((wgs_array)), color='#98d1d1', label="WGS")
@@ -158,6 +160,7 @@ def createLinePlotAneufinder(common_wgs_dict, common_atac_dict):
     plt.show()
 
     plt.show()
+    return(scipy.stats.pearsonr(standarize(atac_array),standarize(smoothed_wgs_array)))
 def createLinePlot(loss_wgs, base_wgs, gain_wgs, loss_atac, base_atac, gain_atac):
     new_base_wgs = [x * 2 for x in base_wgs]
     new_base_atac = [x * 2 for x in base_atac]
@@ -190,7 +193,7 @@ def createLinePlot(loss_wgs, base_wgs, gain_wgs, loss_atac, base_atac, gain_atac
     #plt.plot(x, standarize(wgs_array), color='blue', label="GS")
     #plt.plot(x,standarize(atac_array),color='orange', label="ATAC")
     #both = np.concatenate([standarize(wgs_array)[:, None], standarize(atac_array)[:, None]], axis=1)
-    #x = list(range(len(wgs_array)))
+    #x = list(range(len(w../../Colo320HSP/aneufinder/COLO320HSR_WGS_perBin_noChr.bedgs_array)))
     plt.plot(x, (standarize(atac_array)), color='#df979e', label="ATAC")
     plt.plot(x, (standarize(wgs_array)), color='#98d1d1', label="WGS")
     #plt.plot(both)
@@ -203,9 +206,16 @@ def createLinePlot(loss_wgs, base_wgs, gain_wgs, loss_atac, base_atac, gain_atac
 
 
 if __name__ =="__main__":
+    p = {"filter_edges": "nearest",
+         "atac_input": "/home/katia/Helmholz/epiAneufinder/revisions/GSM4861367_COLO320HSR_rep1_atac/epiAneufinder_results/colo320HSP_rep1_results_table_nochr.tsv",
+         "wgs_reads": "/home/katia/Helmholz/epiAneufinder/Colo320HSP/aneufinder/COLO320HSR_WGS_perBin_noChr.bed",
+         "smooth_sigma": .1,
+         "title": "WGS vs scATAC"}
+    p, _ = fargv.fargv(p)
+    harmonics = {}
     #fin=open("/home/katia/Helmholz/epiAneufinder/Colo320HSP/aneufinder/COLO320_HSR_WGS.CNV_nochr.bed")
-    fin = open("/home/katia/Helmholz/epiAneufinder/Colo320HSP/cnvkit_hmmGermline/COLO320_HSR_WGS.intersect.bed")
-    snu_full=pd.read_csv("/home/katia/Helmholz/epiAneufinder/revisions/GSM4861367_COLO320HSR_rep1_atac/epiAneufinder_results/colo320HSP_rep1_results_table_nochr.tsv", sep=" ")
+    fin = open(p.wgs_reads)
+    snu_full=pd.read_csv(p.atac_input, sep=" ")
     #l = ["seq", "start", "end"]
     #cellCluster = open("/home/katia/Helmholz/epiAneufinder/revisions/GSM4861371_COLO320HSR_rep3_atac/epiAneufinder_results/cluster2_cells.txt", "r")
     #lines = cellCluster.readlines()
@@ -213,10 +223,16 @@ if __name__ =="__main__":
     #    l.append(line.strip())
     #snu_select=snu_full.loc[:,l]
     snu_dict=createDictionaryFromTable(snu_full)
-    bed_dict=createDictionaryFromBedCNVkit(fin)
-    #common_wgs_dict, common_atac_dict = calculatePopulationSomiesWGS(snu_dict,bed_dict)
-    #createLinePlotAneufinder(common_wgs_dict, common_atac_dict)
-    loss_wgs, base_wgs, gain_wgs, loss_atac, base_atac, gain_atac = calculatePopulationSomies(snu_dict, bed_dict)
-    createLinePlot(loss_wgs, base_wgs, gain_wgs, loss_atac, base_atac, gain_atac)
+    bed_dict=createDictionaryFromBed(fin)
+    common_wgs_dict, common_atac_dict = calculatePopulationSomiesWGS(snu_dict,bed_dict)
+    #for sigma in range(1, 10, 1):
+    #    p.smooth_sigma = sigma
+    for _ in (0,):
+        correlation=createLinePlotAneufinder(common_wgs_dict, common_atac_dict,gaussian_sigma=p.smooth_sigma, filter_edges=p.filter_edges)
+        harmonics[p.smooth_sigma] = correlation
+        print(harmonics)
+
+    #loss_wgs, base_wgs, gain_wgs, loss_atac, base_atac, gain_atac = calculatePopulationSomies(snu_dict, bed_dict)
+    #createLinePlotAneufinder(loss_wgs, base_wgs, gain_wgs, loss_atac, base_atac, gain_atac)
     #print(gain_atac)
     #print(loss_wgs,loss_atac)
